6f1fbf2bf238fb8d0d4708193be1d907c45f17c8
Alperen Eroğlu <73244893+aerogluMU@users.noreply.github.com>
2023-08-23 15:19:10 +0300

    Delete unused explanations

--- Qt/mainwindow.cpp
+++ Qt/mainwindow.cpp
@@ -74,16 +74,6 @@ void MainWindow::on_btnConnect_clicked()
 
 }
 
-/*
-During process of making this program, i encountered a problem.
-Library of QSerialPort does not guarantee that received message comes at once.It mostly comes part by part.
-Sometimes it comes completely.But it is not guarantee.I tried to solve this issue by giving message a code.
-I stored every received message in buffer. Whenever i store message in my buffer,i searched my code in buffer.
-If it is found,i display the part of the message until the code. And i remove that message from buffer.
-If it is not found i keep storing received message in buffer.
-*/
-
-
 void MainWindow::receiveMessage()
 {
     QByteArray dataBA = serialPort.readAll();
